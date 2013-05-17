a400b714f6851735e1a1e185f95846d019fff3fd
Alex Chan <alexwlchan@fastmail.co.uk>
2013-05-17 23:59:23 +0100

    Added a simple check that OF exists, ability to change location of OF and fixed printing of newlines.

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-echo '\n Making a backup of omnifocus in /Applications/OmniFocus-backup.app\n'
+APP="/Applications" # Directory where OmniFocus is stored
 
-cp -rf /Applications/OmniFocus.app /Applications/OmniFocus-backup.app
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo ' Copying the new resources \n'
+if [ ! -d $APP/OmniFocus.app/ ];
+then
+	printf "\nOmniFocus.app was not found in the $APP directory.\n"
+else
 
-cp -f OmniFocusInterface/* /Applications/OmniFocus.app/Contents/Frameworks/OmniFocusInterface.framework/Versions/Current/Resources
-cp -f Resources/* /Applications/OmniFocus.app/Contents/Resources/
-cp -f OOview/* /Applications/OmniFocus.app/Contents/Frameworks/OOView.framework/Resources
-cp -f OmniStyle/* /Applications/OmniFocus.app/Contents/Frameworks/OmniStyle.framework/Versions/A/Resources/LeafHandle.pdf
+	printf "\nMaking a backup of OmniFocus to $APP/OmniFocus-backup.app\n"
+	cp -rf $APP/OmniFocus.app $APP/OmniFocus-backup.app
 
-echo " Applying the theme 'omnifocus2.ofocus-theme'. Accept it and restart omnifocus "
-sleep 2
-open ./omnifocus2.ofocus-theme
+	printf "Copying the new resources \n"
 
+	# The first directory is that from the original script; the second is the one that worked for me. ^AWLC
+	cp -f $DIR/OmniFocusInterface/* $APP/OmniFocus.app/Contents/Frameworks/OmniFocusInterface.framework/Versions/Current/Resources
+	cp -f $DIR/OmniFocusInterface/* $APP/OmniFocus.app/Contents/Frameworks/OmniFocusInterface.framework/Resources
 
+	cp -f $DIR/Resources/* $APP/OmniFocus.app/Contents/Resources
+	cp -f $DIR/OOview/* $APP/OmniFocus.app/Contents/Frameworks/OOView.framework/Resources
+	cp -f $DIR/OmniStyle/* $APP/OmniFocus.app/Contents/Frameworks/OmniStyle.framework/Versions/A/Resources/LeafHandle.pdf
 
+	printf "Applying the theme 'omnifocus2.ofocus-theme'. Click 'Replace' and restart OmniFocus.\n\n"
+	sleep 2
+	open $DIR/omnifocus2.ofocus-theme
+fi
